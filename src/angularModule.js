0510fe58540e634cb18c81bd1dd9a35bbbfc75b6
jacobscarter <jacob@ieksolutions.com>
2015-01-30 10:25:35 -0700

    Adding source files

---
+++ src/angularModule.js
@@ -0,0 +1,1 @@
+angular.module('angular-number-incrementor', ['templates-angularnumberincrementor']);
---
+++ src/numberIncrementor.js
@@ -0,0 +1,33 @@
+angular.module('angular-number-incrementor').directive('numberIncrementor', [ function() {
+        
+
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        scope: {
+            output : '='
+        },
+        templateUrl: function(element, attributes) {
+          return attributes.template || "numberincrementor.html";
+        },
+        link: function($scope, $element, $attrs) {
+
+
+            $scope.output = $scope.spinnerValue;
+            
+            $scope.spinnerValue = 0;
+
+            $scope.increase = function(){
+                $scope.spinnerValue++;
+            };
+
+            $scope.decrease = function(){
+                $scope.spinnerValue--;
+            };
+
+            
+
+        }
+    };
+}]);
